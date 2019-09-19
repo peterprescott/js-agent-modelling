1b20c8d54ca8d6433c796b1b036f092c1c3ed8ad
Peter Prescott <peter@prescott.ws>
2019-09-19 17:06:38 +0100

    slow progress defining Agents

--- script.js
+++ script.js
@@ -2,11 +2,45 @@
 
 console.log('connected!')
 
-var data = [3, 7, 2, 9, 1, 11];
-var sum = 0;
-data.forEach(function(d){
-    sum += d;
-});
+let circle = d3.selectAll("circle");
 
-console.log('Sum = ' + sum);
-// outputs 'Sum = 33'
+circle.style("fill", "steelblue");
+circle.attr("r", 30);
+
+circle.attr("cx", function() { return Math.random() * 720; });
+
+let total_agents = 1
+const backdrop = document.getElementById('backdrop')
+
+class Agent {
+  constructor(x, y) {
+    // set initial values
+    this.id = total_agents;
+    total_agents ++;
+    this.x = x;
+    this.y = y;
+    this.step_size = 100;
+
+    // draw Agent
+    backdrop.innerHTML += '<circle cx="' 
+                            + this.x 
+                            + '" cy="' 
+                            + this.y 
+                            + '" r="10" id="circle' 
+                            + this.id 
+                            + '"></circle>';
+}
+    move(){
+        if (Math.random() < 0.5) {this.x = this.x + this.step_size} else {this.x = this.x - this.step_size}
+        if (Math.random() < 0.5) {this.y = this.y + this.step_size} else {this.y = this.y - this.step_size}    
+        
+        console.log('working')
+    
+        
+        
+}
+
+}
+
+
+max = new Agent(100,200)
