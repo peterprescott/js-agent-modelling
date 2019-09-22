e34307f80c742724544a435917e9c60923809665
peterprescott <peter@prescott.ws>
2019-09-22 02:08:49 +0100

    sheep eating grass

--- script.js
+++ script.js
@@ -6,8 +6,7 @@ console.log('connected!')
 const backdrop = document.getElementById('backdrop')
 let total_agents = 0
 let agents = []
-let distances = []
-let num_of_agents = 100
+let num_of_agents = 3
 let step_size = 1
 let neighbourhood = 5
 
@@ -20,6 +19,7 @@ class Agent {
     this.x = x;
     this.y = y;
     this.step_size = step_size;
+    this.store = 0
     agents.push(this)
     }
 
@@ -28,6 +28,14 @@ class Agent {
         if (Math.random() < 0.5) {this.y = (((this.y + this.step_size) % 500) + 500) % 500} else {this.y = (((this.y - this.step_size) % 500) + 500) % 500}    
         this.interact()
     }
+
+	eat(){
+		if (env.matrix[Math.round(this.x/100)][Math.round(this.y/100)] > 0){
+			console.log('eating');
+			env.matrix[Math.round(this.x/100)][Math.round(this.y/100)] = env.matrix[Math.round(this.x/100)][Math.round(this.y/100)] - .1
+		}
+		
+	}
 
     interact(){
         for (let j=0 ; j < agents.length ; j++){ 
@@ -50,7 +58,7 @@ class Environment {
 		for (let x = 0; x < this.width; x++){
 			let row = []
 			for (let y = 0; y < this.height; y++){
-				row.push(Math.round(Math.random()*255))
+				row.push(Math.round(Math.random()*155+100))
 			}
 		this.matrix.push(row)
 		}
@@ -65,7 +73,6 @@ class Environment {
 function distance_between(m,n){
     let distance_squared = (agents[m].x-agents[n].x)**2 + (agents[m].y-agents[n].y)**2
     let distance = distance_squared**(1/2)
-    distances.push(distance)
     return distance
 }
 
@@ -76,7 +83,7 @@ function draw(agents){
     // draw newly-positioned Agents
     for (let i=0; i < agents.length; i++){
         let agent = agents[i];
-        backdrop.innerHTML += '<circle cx="' 
+        backdrop.innerHTML += '<circle fill="white" cx="' 
                                 + agent.x 
                                 + '" cy="' 
                                 + agent.y 
@@ -86,13 +93,18 @@ function draw(agents){
         }
     }
 
-env = new Environment(50,50)
-
+var env = new Environment(6,6)
 function paint(backdrop, env = Environment){
+	// erase pixels
+	var pixels = backdrop.innerHTML = ''
+	
+	// draw newly positioned pixels
 	for (let x = 0; x < env.width; x++){
 			for (let y = 0; y < env.height; y++){
 					//draw env.matrix[x][y] pixel
-					backdrop.innerHTML += '<rect fill="rgb(0,' + env.matrix[x][y] + ',0)" x="' + x*10 + '" y="' + y*10 + '" width="10" height="10" />'
+					let posX = x*100-50
+					let posY = y*100-50
+					backdrop.innerHTML += '<rect fill="rgb(0,' + env.matrix[x][y] + ',0)" x="' + posX + '" y="' + posY + '" width="100" height="100" />'
 			}
 
 		}
@@ -103,23 +115,35 @@ function paint(backdrop, env = Environment){
 
 
 // generate agents
-function generate(){
-    agents = []
-    num_of_agents = Number(document.getElementById('agentnumber').value)
+function generate(num_of_agents){
+	    agents = []
     for (i = 0; i < num_of_agents; i++){
-    new Agent(Math.random()*500, Math.random()*500);
+	    new Agent(Math.random()*500, Math.random()*500);
     }
-    paint(backdrop)
+    paint(backdrop, env)
     draw(agents)
 }
 
 function iterate(){
         for (i = 0; i < agents.length; i++){
             agents[i].move()
+            agents[i].eat()
         }
+		paint(backdrop, env)
         draw(agents)
+
  }
+
+function more(){
+	console.log('still with you')
+	getNumber = Number(document.getElementById('agentnumber').value)
+	console.log('got the number '+getNumber)
+	generate(getNumber)
+
+}
+
 
 generate(num_of_agents)
 
-setInterval(iterate, 1)
+var refreshIntervalId =  setInterval(iterate, 10)
+
